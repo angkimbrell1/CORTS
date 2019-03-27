18cb781c6743f3990d81069ad7523b126b5b617b
kimb7669 <kimb7669@AA-ITS-F115-26>
2019-03-27 11:43:08 -0400

    Connected homepage to backend and created table

--- Corts/Controllers/HomeController.cs
+++ Corts/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Corts.Models;
+﻿using Corts.DAL;
+using Corts.Models;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -12,8 +13,34 @@ namespace Corts.Controllers
 {
     public class HomeController : Controller
     {
+        private Dal dal = new Dal();
         public ActionResult Index(string email)
         {
+            //Get list of current cars in the database
+            List<Cars> currentCarList = new List<Cars>();
+            List<string> typeOfCar = new List<string>();
+            List<double> overallRating = new List<double>();
+            List<double> reliability = new List<double>();
+            List<double> overallCost = new List<double>();
+            List<int> highway = new List<int>();
+            List<int> city = new List<int>();
+            currentCarList = getCurrentCarList();
+            foreach(var car in currentCarList)
+            {
+                typeOfCar.Add(car.type);
+                overallRating.Add(car.OverallRating);
+                reliability.Add(car.Reliability);
+                overallCost.Add(car.OverallCost);
+                highway.Add(car.highway);
+                city.Add(car.city);
+            }
+
+            ViewBag.Car = typeOfCar;
+            ViewBag.MaintenanceCost = overallCost;
+            ViewBag.Highway = highway;
+            ViewBag.City = city;
+            ViewBag.Reliability = reliability;
+            ViewBag.OverallRating = overallRating;
             return View();
         }
 
@@ -24,7 +51,10 @@ namespace Corts.Controllers
             return View();
         }
 
-        
-       
+        public List<Cars> getCurrentCarList()
+        {
+            return dal.getCurrentCarList();
+        }
+
     }
 }
--- Corts/Models/HomeViewModel.cs
+++ Corts/Models/HomeViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,27 @@ namespace Corts.Models
 {
     public class HomeViewModel
     {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "Car")]
+        public string Car { get; set; }
+
+        [JsonProperty(PropertyName = "Maintenance Cost")]
+        public int MaintenanceCost { get; set; }
+
+        [JsonProperty(PropertyName = "Highway")]
+        public int Highway { get; set; }
+
+        [JsonProperty(PropertyName = "City")]
+        public int City { get; set; }
+
+        [JsonProperty(PropertyName = "Overall Rating")]
+        public double OverallRating { get; set; }
+
+        [JsonProperty(PropertyName = "Reliability")]
+        public double Reliability { get; set; }
+        
+        
     }
 }
