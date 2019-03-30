9c8bc3a0ce9a8816cebcd6fff0be5995fea68f14
kimb7669 <kimb7669@AA-ITS-F115-26>
2019-03-30 18:06:16 -0400

    Merging Mike and I's changes...

--- Corts/DAL/dal.cs
+++ Corts/DAL/dal.cs
@@ -7,6 +7,7 @@ using MongoDB.Driver;
 using MongoDB.Bson;
 using System.Configuration;
 using System.Security.Authentication;
+using System.Security.Cryptography;
 using MongoDB.Driver.Linq;
 using static Corts.Models.Classes;
 
@@ -21,6 +22,7 @@ namespace Corts.DAL
         private string userName = "";
         private string host = "";
         private string password = "";
+
 
 
         private string dbName = "CortsDB";
@@ -135,7 +137,6 @@ namespace Corts.DAL
         //Login user
         public bool LoginUser(Users user)
         {
-            //We still need unhashing the passwords - but first hash passwords upon registration
             var collection = GetUsersCollection();
             string email = user.email;
             string password = user.password;
@@ -148,12 +149,46 @@ namespace Corts.DAL
             {
                 return false;
             }
+
+            //Get savedPasswordHash from the data base
             string passwordFromDB = list[0].password;
 
-            if (passwordFromDB == password)
+            //Convert the password from the DB to an array of bytes
+            byte[] hashBytes = Convert.FromBase64String(passwordFromDB);
+
+            //Get the salt from the hashbytes
+            byte[] salt = new byte[16];
+            Array.Copy(hashBytes, 0, salt, 0, 16);
+
+            //Use the same variable to hash the password that the user entered
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000);
+
+            //Convert the hashed password from the user to an array of bytes
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            //Set a flag 'ok' to validate the password that the user entered
+            //with the password in the database
+            int ok = 1;
+
+            //Loop that checks the validity of the password by comparing each byte
+            //of the db password with the user password. The hashBytes starts at 16
+            //because the salt value is stored in the first 16 bytes and we aren't
+            //comparing that with anything.
+            for (int i = 0; i < 20; i++)
+            {
+                if (hashBytes[i + 16] != hash[i])
+                {
+                    ok = 0;
+                }
+            }
+
+            //log the user in
+            if (ok == 1)
             {
                 return true;
             }
+
+            //Deny the user
             else
             {
                 return false;
@@ -162,17 +197,52 @@ namespace Corts.DAL
         //Check User Registration Date
         public bool getCreatedDate(Users user)
         {
-            //We still need unhashing the passwords - but first hash passwords upon registration
             var collection = GetUsersCollection();
             string email = user.email;
             string password = user.password;
 
+            //Get savedPasswordHash from the data base
             var builder = Builders<Users>.Filter;
             var filt = builder.Where(x => x.email == email);
             var list = collection.Find(filt).ToList();
+            string passwordFromDB = list[0].password;
+
+            //Convert the password from the DB to an array of bytes
+            byte[] hashBytes = Convert.FromBase64String(passwordFromDB);
+
+            //Get the salt from the hashbytes
+            byte[] salt = new byte[16];
+            Array.Copy(hashBytes, 0, salt, 0, 16);
+
+            //Use the same variable to hash the password that the user entered
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000);
+
+            //Convert the hashed password from the user to an array of bytes
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            //Set a flag 'ok' to validate the password that the user entered
+            //with the password in the database
+            int ok = 1;
+
+            //Loop that checks the validity of the password by comparing each byte
+            //of the db password with the user password. The hashBytes starts at 16
+            //because the salt value is stored in the first 16 bytes and we aren't
+            //comparing that with anything.
+            for (int i = 0; i < 20; i++)
+            {
+                if (hashBytes[i + 16] != hash[i])
+                {
+                    ok = 0;
+                }
+            }
+
             string TodaysDate = DateTime.Today.ToString();
 
             if (list.Count == 0)
+            {
+                return false;
+            }
+            else if (ok == 0)
             {
                 return false;
             }
@@ -185,12 +255,31 @@ namespace Corts.DAL
                 return false;
             }
         }
-
         //Register User
         public bool CreateUser(Users user)
         {
-           
-            //TODO: HASH PASSWORDS UPON REGISTRATION
+            //New byte array to hold the salt
+            byte[] salt;
+
+            //Generate a random salt
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+
+            //Concatenate salt and user password then hash it using Rfc...Bytes
+            var pdkdf2 = new Rfc2898DeriveBytes(user.password, salt, 10000);
+
+            //Place the concatenated string in the byte array hash
+            byte[] hash = pdkdf2.GetBytes(20);
+
+            //New byte array to store hashed password + salt
+            byte[] hashBytes = new byte[36];
+
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            string savedPasswordHash = Convert.ToBase64String(hashBytes);
+
+            user.password = savedPasswordHash;
+
             var collection = GetUsersCollectionForEdit();
             try
             {
@@ -202,7 +291,6 @@ namespace Corts.DAL
             }
             return true;
         }
-       
 
 
         //Database Collection Retrievers
